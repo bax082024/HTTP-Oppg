bba4f3643d2621801e5caf55c5546fc9cfba5c81
bax082024 <bax082024@gmail.com>
2024-11-05 06:14:29 +0100

    added get request and read response

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ namespace HttpFun
 {
   class Program
   {
-    static async
+    static async Task Main(string[] args)
+    {
+      string url = "https://official-joke-api.appspot.com/random_joke";
+
+      using HttpClient client = new HttpClient();
+
+      try 
+      {
+
+        // Send Get Request
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        //Read The response as string
+        string responseBody = await response.Content.ReadAsStringAsync();
+      }
+    }
   }
 }
--- obj/Debug/net8.0/Http-Oppg.AssemblyInfo.cs
+++ obj/Debug/net8.0/Http-Oppg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Http-Oppg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9414fa67a307b26b8fabb61dd129b48b9a39c358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12dd72a8ffa6a60a3bda1c455ec8fe548d5d0385")]
 [assembly: System.Reflection.AssemblyProductAttribute("Http-Oppg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Http-Oppg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
